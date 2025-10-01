6d6e136ab351cef364dfa06ff10da63e33a5a60c
Loboa <loboav05@gamil.com>
2025-10-01 17:49:30 +0300

    Добавлено файл .gitattributes для обеспечения LF

--- graph-visualization-app/db/init.sql
+++
@@ -1,6 +0,0 @@
-# Удалите или закомментируйте строку с init.sql в docker-compose.yml, чтобы Postgres не завершал работу сразу после старта.
-# Это решит проблему с "db" контейнером, который сразу выключается.
-
-# Пример:
-#    volumes:
-#      # - ./db/init.sql:/docker-entrypoint-initdb.d/init.sql
